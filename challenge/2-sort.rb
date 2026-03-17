976814ed32f77008d763436cd2a189559dab0230
gunelismayilzada <gunelismayilzad7@gmail.com>
2026-03-17 18:29:21 +0400

    fixx

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,16 +1,31 @@
-#!/usr/bin/env ruby
+###
+#
+#  Sort integer arguments (ascending)
+#
+###
 
-args = ARGV.map do |a|
-  begin
-    Integer(a)  # Əgər a ədəd olarsa, integer-ə çevir
-  rescue
-    a           # Əks halda string kimi saxla
-  end
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+    # convert to integer
+    i_arg = arg.to_i
+    # insert result at the right position
+    result << i_arg
 end
 
-# Sırala: əvvəlcə ədədlər artan, sonra strings (əlavə olaraq lazım deyilsə)
-sorted = args.sort do |a, b|
-  a <=> b   # `<=>` operatoru integerlər üçün düzgün artan sıra yaradır
+n = result.length
+loop do
+    swapped = false
+
+    (n-1).times do |i|
+        if result[i] < result[i+1]
+            result[i], result[i+1] = result[i+1], result[i]
+            swapped = true
+        end
+    end
+
+    break if not swapped
 end
 
-puts sorted
+puts result
